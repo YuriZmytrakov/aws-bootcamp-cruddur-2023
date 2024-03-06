d48f9c3ed22a1908e4d8ba74aac58e3112453fca
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2024-03-06 00:09:27 +0000

    lambda part

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs'; // Added RemovalPolicy
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from 'dotenv';
+
+// const dotenv = require('dotenv');
+dotenv.config();
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const bucket = this.createBucket(bucketName);
+    const lambda = this.createLambda(functionPath);
+  }
+
+  createBucket(bucketName: string): s3.Bucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+
+  createLambda(functionPath: string): lambda.IFunction {
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+    });
+    return lambdaFunction;
+  }
+}
